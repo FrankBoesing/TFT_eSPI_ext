c5c61d4a2dadcfa8d86a40e04aa651ba3d646a97
Bodmer <bodmer@anola.net>
2022-01-11 13:36:52 +0000

    Make example Arduino IDE compatible
    If example is in folder named  zzyx then sketch must be called zzyx.ino

--- examples/WT32-SC01-test-ttf-gradients/src/main.cpp
+++ examples/WT32-SC01-test-ttf-gradients/src/main.cpp
@@ -1,23 +1,25 @@
 
-
-#include <TFT_eSPI.h>
 #include <TFT_eSPI_ext.h>
 
-#include "screens.h"
-
+// The following comment is more relevant to enhancing the library?
+// If these can be included in a sketch then a "how to" is needed.
 // More than 900 Fonts available here:
 // https://github.com/FrankBoesing/fonts/tree/master/ofl
 
-//TFT_eSPI tft = TFT_eSPI();
 TFT_eSPI_ext tft = TFT_eSPI_ext();
 
+#include "screens.h" // Must include after tft instance created
 
+/*
+Things are undefined here in the Arduino environment - maybe was in platformio.ini?
+TFT_eSPI will switch on backlight so delete this and leave user to add own code
 void setBacklight(char brightness) //0..100
 {
     const float fact = ((1 << BACKLIGHT_RESOLUTION) - 1) / 100.0f;
     if (brightness > 100) brightness = 100;
     ledcWrite(BACKLIGHT_CHANNEL, fact * brightness );
 }
+*/
 
 void setup() {
 
@@ -26,13 +28,18 @@ void setup() {
   Serial.println("TFT_eSPI_ext library test!");
 
   tft.init();
-  // tft.fillScreen(TFT_BLACK);
+  tft.fillScreen(TFT_BLACK);
   tft.setRotation(1);
 
+/*
+Things are undefined here in the Arduino environment
+TFT_eSPI will switch on backlight so delete this and leave user to add own code
 
   ledcSetup(BACKLIGHT_CHANNEL, BACKLIGHT_FREQ, BACKLIGHT_RESOLUTION);
   ledcAttachPin(TFT_BL, BACKLIGHT_CHANNEL);
   setBacklight(20);
+*/
+
   Screen2();
 }
 

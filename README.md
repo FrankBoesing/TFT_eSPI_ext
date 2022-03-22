3481e40f05c0c8f6d700c6d0891652e31bd04650
Bodmer <bodmer@anola.net>
2022-03-22 22:32:59 +0000

    Support font rendering in sprites, API change
    Examples have been updated for the new API.

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Extensions to the [TFT_eSPI](https://github.com/Bodmer/TFT_eSPI) library by bodm
 Font code mostly taken from ILI9341_t3 library by Paul Stoffregen,
 then slightly optimized.
 
+Note: At v 0.1.1 the API changed and this is not longer compatible with previous versions. The examples have been updated. Fonts can be rendered to the screen or a sprite.
+
 Fonts:
 
 https://github.com/PaulStoffregen/ILI9341_fonts
--- examples/Font_test/Font_test.ino
+++ examples/Font_test/Font_test.ino
@@ -1,75 +1,85 @@
 // Test the font rendering with and without background colour
 
 #include <SPI.h>
-#include <TFT_eSPI_ext.h>
+#include <TFT_eSPI.h>
+#include <TFT_eSPI_ext.h> // Include font extension library
 
 // Fonts in these files are available in the following sizes:
 // 8, 9, 10, 11, 12, 13, 14, 16, 18, 20, 24, 28, 32, 40, 48, 60, 72 and 96
 // This example uses size 32
-
 #include <font_Arial.h>
 #include <font_ArialBold.h>
 #include <font_ArialItalic.h>
 #include <font_ArialBoldItalic.h>
 
-TFT_eSPI_ext tft = TFT_eSPI_ext();
+TFT_eSPI     tft = TFT_eSPI();
+TFT_eSprite  img = TFT_eSprite(&tft);
+
+// TFT_eSPI_ext will inherit the TFT_eSPI class functions so the created "etft" instance
+// can call both the extension font class and the "tft" instance TFT_eSPI base class functions.
+// So tft.fillScreen(TFT_BLUE) and etft.fillScreen(TFT_BLUE) do the same thing!
+// TFT_eSPI_ext functions can only be called using the "etft" instance
+TFT_eSPI_ext etft = TFT_eSPI_ext();
 
 uint16_t ypos = 0;
   
 void setup() {
   Serial.begin(115200);
-  tft.begin();
-  tft.setRotation(1);
+  etft.begin();
+  etft.setRotation(1);
 
-  tft.fillScreen(TFT_BLACK);
+  etft.fillScreen(TFT_BLACK);
   Serial.println("Font test"); 
 
-  tft.setTextColor(TFT_YELLOW);
+  // Set TFT_eSPI_ext to render font on the TFT
+  etft.TTFdestination(&tft);
 
-  tft.setTTFFont(Arial_32);
-  tft.setCursor(0, ypos);
-  tft.print("Arial");
-  ypos += tft.TTFLineSpace();
+  etft.setTextColor(TFT_YELLOW);
 
-  tft.setCursor(0, ypos);
-  tft.setTTFFont(Arial_32_Bold);
-  tft.print("Arial Bold");
-  ypos += tft.TTFLineSpace();
+  etft.setTTFFont(Arial_32);
+  etft.setCursor(0, ypos);
+  etft.print("Arial");
+  ypos += etft.TTFLineSpace();
 
-  tft.setCursor(0, ypos);
-  tft.setTTFFont(Arial_32_Italic);
-  tft.print("Arial Italic");
-  ypos += tft.TTFLineSpace();
+  etft.setCursor(0, ypos);
+  etft.setTTFFont(Arial_32_Bold);
+  etft.print("Arial Bold");
+  ypos += etft.TTFLineSpace();
 
-  tft.setCursor(0, ypos);
-  tft.setTTFFont(Arial_32_Bold_Italic);
-  tft.print("Arial Bold Italic");
+  etft.setCursor(0, ypos);
+  etft.setTTFFont(Arial_32_Italic);
+  etft.print("Arial Italic");
+  ypos += etft.TTFLineSpace();
+
+  etft.setCursor(0, ypos);
+  etft.setTTFFont(Arial_32_Bold_Italic);
+  etft.print("Arial Bold Italic");
 
   delay(3000);
-  tft.setTextColor(TFT_YELLOW, TFT_BLUE);
-  tft.fillScreen(TFT_BLACK);
+  etft.setTextColor(TFT_YELLOW, TFT_BLUE);
+  etft.fillScreen(TFT_BLACK);
 
   delay(1000);
   ypos = 0;
-  tft.setTTFFont(Arial_32);
-  tft.setCursor(0, ypos);
-  tft.print("Arial");
-  ypos += tft.TTFLineSpace();
+  etft.setTTFFont(Arial_32);
+  etft.setCursor(0, ypos);
+  etft.print("Arial");
+  ypos += etft.TTFLineSpace();
 
-  tft.setCursor(0, ypos);
-  tft.setTTFFont(Arial_32_Bold);
-  tft.print("Arial Bold");
-  ypos += tft.TTFLineSpace();
+  etft.setCursor(0, ypos);
+  etft.setTTFFont(Arial_32_Bold);
+  etft.print("Arial Bold");
+  ypos += etft.TTFLineSpace();
 
-  tft.setCursor(0, ypos);
-  tft.setTTFFont(Arial_32_Italic);
-  tft.print("Arial Italic");
-  ypos += tft.TTFLineSpace();
+  etft.setCursor(0, ypos);
+  etft.setTTFFont(Arial_32_Italic);
+  etft.print("Arial Italic");
+  ypos += etft.TTFLineSpace();
 
-  tft.setCursor(0, ypos);
-  tft.setTTFFont(Arial_32_Bold_Italic);
-  tft.print("Arial Bold Italic");
-  ypos += tft.TTFLineSpace();
+  etft.setCursor(0, ypos);
+  etft.setTTFFont(Arial_32_Bold_Italic);
+  etft.print("Arial Bold Italic");
+  ypos += etft.TTFLineSpace();
 
   delay(3000);
   tft.fillScreen(TFT_BLACK);
@@ -77,14 +87,14 @@ void setup() {
 
 void loop(void) {
   // With background colour to erase ol  values
-  tft.setTextColor(TFT_YELLOW, TFT_BLUE);
-  tft.setTTFFont(Arial_32_Bold);
+  etft.setTextColor(TFT_YELLOW, TFT_BLUE);
+  etft.setTTFFont(Arial_32_Bold);
 
   for (int i = 100; i < 1000; i++ )
   {
-    tft.setCursor(0, 0);
-    tft.print(i);
-    tft.print(" "); // Add space to clear stray pixels since not all glyphs are the same width
+    etft.setCursor(0, 0);
+    etft.print(i);
+    etft.print(" "); // Add space to clear stray pixels since not all glyphs are the same width
   }
 
   delay(2000);
